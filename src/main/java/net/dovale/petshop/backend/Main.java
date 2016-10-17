3094db24997fc7bd032423b32abd71b85d3428af
Raphael do Vale Amaral Gomes <raphaeldovale@outlook.com>
2016-10-16 22:16:22 -0200

    Estado inicial para projeto da petshot

---
+++ src/main/java/net/dovale/petshop/backend/Main.java
@@ -0,0 +1,14 @@
+package net.dovale.petshop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Main {
+
+	public static void main(String[] args){
+		SpringApplication.run(Main.class,args);
+	}
+}
